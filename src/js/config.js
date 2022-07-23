3b0418fa7ffaa7c6c7677552a5c47cfd6c9145c3
AlexaNem0 <vikalyeksyeyev@gmail.com>
2022-07-23 08:09:12 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3a769efb-23c6-4061-88c6-78730ecefadd';
+export const MODA_CLOSE_SEC = 2.5;
